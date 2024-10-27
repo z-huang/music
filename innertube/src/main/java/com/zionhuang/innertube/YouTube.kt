a7bc0f480b01e0db4cbc1d2bdb3639ad8b55b5ae
gechoto <124326167+gechoto@users.noreply.github.com>
2024-10-27 18:00:34 +0100

    try `ANDROID_MUSIC` client first for logged in users

--- innertube/src/main/java/com/zionhuang/innertube/YouTube.kt
+++ innertube/src/main/java/com/zionhuang/innertube/YouTube.kt
@@ -417,7 +417,14 @@ object YouTube {
     }
 
     suspend fun player(videoId: String, playlistId: String? = null): Result<PlayerResponse> = runCatching {
-        val playerResponse = innerTube.player(IOS, videoId, playlistId).body<PlayerResponse>()
+        var playerResponse: PlayerResponse
+        if (this.cookie != null) { // if logged in: try ANDROID_MUSIC client first because IOS client does not play age restricted songs
+            playerResponse = innerTube.player(ANDROID_MUSIC, videoId, playlistId).body<PlayerResponse>()
+            if (playerResponse.playabilityStatus.status == "OK") {
+                return@runCatching playerResponse
+            }
+        }
+        playerResponse = innerTube.player(IOS, videoId, playlistId).body<PlayerResponse>()
         if (playerResponse.playabilityStatus.status == "OK") {
             return@runCatching playerResponse
         }
