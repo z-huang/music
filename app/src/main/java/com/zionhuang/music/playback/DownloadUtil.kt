048ec31e5dfe0c6c0afb0dec03c598fb8638f316
gechoto <124326167+gechoto@users.noreply.github.com>
2024-12-21 19:45:31 +0100

    add yt-dlp fallback for downloads

--- app/src/main/java/com/zionhuang/music/playback/DownloadUtil.kt
+++ app/src/main/java/com/zionhuang/music/playback/DownloadUtil.kt
@@ -20,6 +20,7 @@ import com.zionhuang.music.db.MusicDatabase
 import com.zionhuang.music.db.entities.FormatEntity
 import com.zionhuang.music.di.DownloadCache
 import com.zionhuang.music.di.PlayerCache
+import com.zionhuang.music.utils.YoutubeDL
 import com.zionhuang.music.utils.enumPreference
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
@@ -88,10 +89,7 @@ class DownloadUtil @Inject constructor(
                             AudioQuality.LOW -> -1
                         } + (if (it.mimeType.startsWith("audio/webm")) 10240 else 0) // prefer opus stream
                     }
-            }!!.let {
-                // Specify range to avoid YouTube's throttling
-                it.copy(url = "${it.url}&range=0-${it.contentLength ?: 10000000}")
-            }
+            }!!
 
         database.query {
             upsert(
@@ -108,8 +106,13 @@ class DownloadUtil @Inject constructor(
             )
         }
 
-        songUrlCache[mediaId] = format.url!! to playerResponse.streamingData!!.expiresInSeconds * 1000L
-        dataSpec.withUri(format.url!!.toUri())
+        val streamUrl = (format.url ?: YoutubeDL.getStreamUrl(mediaId, format.itag, YouTube.cookie != null)).let {
+            // Specify range to avoid YouTube's throttling
+            "${it}&range=0-${format.contentLength ?: 10000000}"
+        }
+
+        songUrlCache[mediaId] = streamUrl to playerResponse.streamingData!!.expiresInSeconds * 1000L
+        dataSpec.withUri(streamUrl.toUri())
     }
     val downloadNotificationHelper = DownloadNotificationHelper(context, ExoDownloadService.CHANNEL_ID)
     val downloadManager: DownloadManager = DownloadManager(context, databaseProvider, downloadCache, dataSourceFactory, Executor(Runnable::run)).apply {
