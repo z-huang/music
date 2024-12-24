0bd6e0012c89fb496c932554250c7cfd3622a358
gechoto <124326167+gechoto@users.noreply.github.com>
2024-12-24 06:26:05 +0100

    DownloadUtil: always add range param

--- app/src/main/java/com/zionhuang/music/playback/DownloadUtil.kt
+++ app/src/main/java/com/zionhuang/music/playback/DownloadUtil.kt
@@ -88,10 +88,7 @@ class DownloadUtil @Inject constructor(
                             AudioQuality.LOW -> -1
                         } + (if (it.mimeType.startsWith("audio/webm")) 10240 else 0) // prefer opus stream
                     }
-            }!!.let {
-                // Specify range to avoid YouTube's throttling
-                it.copy(url = "${it.url}&range=0-${it.contentLength ?: 10000000}")
-            }
+            }!!
 
         database.query {
             upsert(
@@ -108,7 +105,10 @@ class DownloadUtil @Inject constructor(
             )
         }
 
-        val streamUrl = format.findUrl()
+        val streamUrl = format.findUrl()?.let {
+            // Specify range to avoid YouTube's throttling
+            "${it}&range=0-${format.contentLength ?: 10000000}"
+        }
 
         songUrlCache[mediaId] = streamUrl!! to playerResponse.streamingData!!.expiresInSeconds * 1000L
         dataSpec.withUri(streamUrl.toUri())
