32398960ae1dff0a1ed33e83301e853e99173a33
gechoto <124326167+gechoto@users.noreply.github.com>
2025-01-08 23:18:28 +0000

    innertube: PlayerResponse: clean up imports

--- innertube/src/main/java/com/zionhuang/innertube/models/response/PlayerResponse.kt
+++ innertube/src/main/java/com/zionhuang/innertube/models/response/PlayerResponse.kt
@@ -2,11 +2,7 @@ package com.zionhuang.innertube.models.response
 
 import com.zionhuang.innertube.models.ResponseContext
 import com.zionhuang.innertube.models.Thumbnails
-import io.ktor.http.URLBuilder
-import io.ktor.http.parseQueryString
 import kotlinx.serialization.Serializable
-import org.schabi.newpipe.extractor.exceptions.ParsingException
-import org.schabi.newpipe.extractor.services.youtube.YoutubeJavaScriptPlayerManager
 
 /**
  * PlayerResponse with [com.zionhuang.innertube.models.YouTubeClient.WEB_REMIX] client
